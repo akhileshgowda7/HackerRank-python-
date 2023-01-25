6933e4384723650f2b4dfbb754be5111774126a2
akhileshgowda <akhileshgowda7@gmail.com>
2023-01-25 16:28:16 -0500

    candles hancherrank

---
+++ candles.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    candles.sort(reverse=True)
+    max_number = candles[0]
+    count = 0
+    print(candles)
+    for i in range(len(candles)):
+        if candles[i]==max_number:
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
