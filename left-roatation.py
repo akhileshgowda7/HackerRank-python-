8e596ebc5951c570cb148d9e497befc67190a9a8
akhileshgowda <akhileshgowda7@gmail.com>
2023-03-02 16:16:33 -0500

    Hackerrank left rotation

---
+++ left-roatation.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+def rotLeft(a, d):
+    # Write your code here
+    temp = []
+    ans = []
+    for i in range(len(a)):
+        if i <d:
+            temp.append(a[i])
+        else:
+            ans.append(a[i])
+    print(ans)
+    for i in range(len(temp)):
+        ans.append(temp[i])
+    
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
